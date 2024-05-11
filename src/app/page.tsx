a33cc8d9640b11f117e7ef0b78babf97411273a6
Matthew Haryanto <matthewanh@hotmail.com>
2024-05-11 00:32:19 -0700

    chore: some minor changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,18 +9,37 @@ import {
   VStack,
   keyframes,
 } from "@chakra-ui/react";
+import { motion } from "framer-motion";
 import { ReactChild } from "react";
 
 const Home = () => {
   return (
     <Box>
-      <Center>
-        <Flex alignItems={"center"} mx={4} height={"100vh"}>
-          <Title />
-          <Picture />
-        </Flex>
-      </Center>
+      <motion.div
+        initial={"hidden"}
+        whileInView={"visible"}
+        exit={"hidden"}
+        viewport={{ once: true }}
+        transition={{ duration: 0.1 }}
+        variants={{
+          visible: { opacity: 1, scale: 1 },
+          hidden: { opacity: 0, scale: 0 },
+        }}
+      >
+        <HomeRaw />
+      </motion.div>
     </Box>
+  );
+};
+
+const HomeRaw = () => {
+  return (
+    <Center>
+      <Flex alignItems={"center"} mx={4} height={"100vh"}>
+        <Title />
+        <Picture />
+      </Flex>
+    </Center>
   );
 };
 
@@ -29,17 +48,18 @@ const Title = () => {
   return (
     <VStack align={"end"} mx={6}>
       <Heading as="h1" fontSize={"2xl"} lineHeight="tall" color={"primary.200"}>
-        <Character color={"primary.100"} char={"const"} />{" "}
-        <Character color={"primary.300"} char={"name:"} />{" "}
-        <Character color={"primary.400"} char={"String"} />{" "}
-        <Character color={"primary.500"} char={"="} />{" "}
+        <Character color={"primary.100"} char={"const "} />
+        <Character color={"primary.300"} char={"name: "} />
+        <Character color={"primary.400"} char={"String"} />
+        <Character color={"primary.500"} char={" = "} />
         <Character color={"primary.600"} char={"'"} />
-        <Character color={"text.main"} char={"Matthew Haryanto"} />
+        <Character color={"text.main"} char={"MATTHEW HARYANTO"} />
         <Character color={"primary.600"} char={"'"} />
+        <Character color={"primary.200"} char={";"} />
       </Heading>
       <AnimatedText />
       <Heading as="h2" fontSize={"2xl"} lineHeight="tall" color={"primary.600"}>
-        Developer&#33;
+        {"<!--- Developer --->"}
       </Heading>
     </VStack>
   );
